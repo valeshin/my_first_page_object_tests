020f3d6933a0e8d9b04a41f01bc920814fcff2bb
valeshin <v.aleshin92@gmail.com>
2021-04-02 02:27:15 +0300

    added parametrize in test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -1,13 +1,14 @@
 from .pages.product_page import ProductPage
-import time
+import pytest
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+@pytest.mark.parametrize('code',
+                         ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
+def test_guest_can_add_product_to_basket(browser, code):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{code}"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
     page.goods_should_be_added_to_basket()
     page.cost_of_basket_is_correct()
-
