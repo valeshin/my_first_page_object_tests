ea86f058d8f27982c35abdd28b520ead6343ed9b
valeshin <v.aleshin92@gmail.com>
2021-04-04 19:55:05 +0300

    added class TestLoginFromMainPage

--- test_main_page.py
+++ test_main_page.py
@@ -21,7 +21,6 @@ class TestLoginFromMainPage():
         page.should_be_login_link()
 
 
-
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
