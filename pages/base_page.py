d948ce2a1c7ccc3221c2f758fd597e10712ef0da
valeshin <v.aleshin92@gmail.com>
2021-04-04 21:57:42 +0300

    return implicitly_wait

--- pages/base_page.py
+++ pages/base_page.py
@@ -12,7 +12,7 @@ class BasePage():
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
-        # self.browser.implicitly_wait(timeout)
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
@@ -69,4 +69,3 @@ class BasePage():
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
-
