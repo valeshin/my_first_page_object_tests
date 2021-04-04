7900a2b3d32703610082c23378a0fabe3fb473a6
valeshin <v.aleshin92@gmail.com>
2021-04-04 21:42:52 +0300

    changed link

--- test_main_page.py
+++ test_main_page.py
@@ -3,11 +3,12 @@ from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 import pytest
 
+link = "http://selenium1py.pythonanywhere.com/"
+
 
 @pytest.mark.login_guest
 class TestLoginFromMainPage():
     def test_guest_can_go_to_login_page(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/"
         page = MainPage(browser, link)
         page.open()
         page.go_to_login_page()
@@ -15,14 +16,12 @@ class TestLoginFromMainPage():
         login_page.should_be_login_page()
 
     def test_guest_should_see_login_link(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/"
         page = MainPage(browser, link)
         page.open()
         page.should_be_login_link()
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
     page.go_to_basket()
