527f5aac05b087650dc4fc2b078602a4ef98dfbf
valeshin <v.aleshin92@gmail.com>
2021-04-04 20:56:19 +0300

    added class TestUserAddToBasketFromProductPage

--- test_product_page.py
+++ test_product_page.py
@@ -2,9 +2,38 @@ from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 import pytest
+import time
 
 
-'''
+@pytest.mark.user_groupe
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        email = str(time.time()) + "@fakemail.org"
+        password = "qwertasdfg12"
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.register_new_user(email, password)
+        login_page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_basket()
+        page.item_should_be_added_to_basket()
+        page.cost_of_basket_is_correct()
+
+
 @pytest.mark.parametrize('number',
                          ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
 def test_guest_can_add_product_to_basket(browser, number):
@@ -15,7 +44,6 @@ def test_guest_can_add_product_to_basket(browser, number):
     page.solve_quiz_and_get_code()
     page.item_should_be_added_to_basket()
     page.cost_of_basket_is_correct()
-'''
 
 
 @pytest.mark.xfail
